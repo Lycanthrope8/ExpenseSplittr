a09c7a6e48b5367c37d1c4cad189d7812e59eecc
Jubayer Hossain <jubayerhossain9999@gmail.com>
2024-03-15 02:06:33 +0600

    userProfile Controller and user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,19 +2,20 @@ const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, {expiresIn: "1d"})
-}
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+};
 
 // Login User
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.login(email, password);
-        const token = createToken(user._id);
-        res.status(200).json({email, token});
-    } catch (error){
-        res.status(400).json({error: error.message});
-    }
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const userId = user._id;
+    const token = createToken(userId);
+    res.status(200).json({ userId, email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //Signup User
@@ -22,11 +23,36 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    const token = createToken(user._id);
-    res.status(201).json({email, token});
-  } catch (error){
-    res.status(400).json({error: error.message});
+    const userId = user._id;
+    const token = createToken(userId);
+    res.status(200).json({ userId, email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 };
 
-module.exports = { loginUser, signupUser };
+
+// Get User Profile
+const getUserProfile = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await User.findById(userId); 
+    res.status(200).json(user);
+  }catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// Update User
+const updateUserProfile = async (req, res) => {
+  const userId = req.params.userId;
+  const { name, address, avatar } = req.body;
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, { name, address, avatar }, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { loginUser, signupUser, getUserProfile, updateUserProfile };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 //Controller functions
-const {loginUser, signupUser} = require('../controllers/userController');
+const {loginUser, signupUser, getUserProfile, updateUserProfile} = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -12,5 +12,12 @@ router.post('/login', loginUser);
 
 // Signup Route
 router.post('/signup', signupUser);
+
+// User Profile Route
+router.get('/profile/:userId', getUserProfile);
+
+// Update User Profile Route
+router.patch('/profile/:userId', updateUserProfile);
+
 
 module.exports = router;
