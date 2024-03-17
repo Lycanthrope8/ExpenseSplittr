8cd287b1e424014b14f6e93ebc30bacb28e82954
Nocktowl <mehedi.hasanhulknabil@gmail.com>
2024-03-17 20:34:44 +0600

    login signup

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -11,21 +11,27 @@ const Login = () => {
   };
 
   return (
-    <form className="login" onSubmit={handleSubmit}>
-      <h3>Log In</h3>
+    <form className="login flex flex-col items-center justify-center min-h-full px-4 py-8 bg-secondary-dark-bg w-1/4 mx-auto" onSubmit={handleSubmit}>
+      <h3 className="text-zinc-100 text-4xl mb-12">Log In</h3>
 
-      <label>Email address:</label>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
+      <div className="flex justify-evenly mb-4 w-full">
+        <label className="flex items-center text-zinc-100 text-xl ml-0">Email:</label>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          className="bg-tertiary-dark-bg text-zinc-200 rounded-xl p-2 w-3/4"
+        />
+      </div>
+      <div className="flex justify-evenly mb-4 w-full">
+        <label className="flex items-center text-zinc-100 text-xl ml-0">Password:</label>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          className="bg-tertiary-dark-bg text-zinc-200 rounded-xl p-2 w-3/4"
+        />
+      </div>
 
       <button disabled={isLoading}>Log in</button>
         {error && <div className="error">{error}</div>}
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -13,23 +13,29 @@ const Signup = () => {
   }
 
   return (
-    <form className="signup" onSubmit={handleSubmit}>
-      <h3>Sign Up</h3>
+    <form className="signup  flex flex-col items-center justify-center min-h-full px-4 py-8 bg-secondary-dark-bg w-1/4 mx-auto" onSubmit={handleSubmit}>
+      <h3 className="text-zinc-100 text-4xl mb-12">Sign Up</h3>
       
-      <label>Email address:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
-      />
-      <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
-      />
+      <div className="flex justify-evenly mb-4 w-full">
+        <label className="flex items-center text-zinc-100 text-xl ml-0">Email:</label>
+        <input 
+          type="email" 
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email}
+          className="bg-tertiary-dark-bg text-zinc-200 rounded-xl p-2 w-3/4"
+        />
+      </div>
+      <div className="flex justify-evenly mb-4 w-full">
+        <label className="flex items-center text-zinc-100 text-xl ml-0">Password:</label>
+        <input 
+          type="password" 
+          onChange={(e) => setPassword(e.target.value)} 
+          value={password} 
+          className="bg-tertiary-dark-bg text-zinc-200 rounded-xl p-2 w-3/4"
+        />
+      </div>
 
-      <button disabled={isLoading}>Sign up</button>
+      <button className="" disabled={isLoading}>Sign up</button>
       {error && <div className="error">{error}</div>}
     </form>
   )
