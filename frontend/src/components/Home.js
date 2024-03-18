de4dc6606e8222e77dca58f5d9f3c4b4d8f77093
lycanthrope8 <jubayerhossain9999@gmail.com>
2024-03-19 04:12:30 +0600

    Join By Code Almost Done

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,17 +2,28 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useProfileContext } from "../hooks/useProfileContext";
-
+import { useNavigate } from "react-router-dom";
 export const Home = () => {
   const { user } = useAuthContext();
   const { profile } = useProfileContext();
   const [loading, setLoading] = useState(true);
+  const [enteredCode, setEnteredCode] = useState(false);
+  const [codeText, setCodeText] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (user && profile) {
       setLoading(false);
     }
   }, [user, profile]);
+
+  const handleJoinClick = () => {
+    setEnteredCode(!enteredCode);
+  };
+
+  const handleSeeDetails = () => {
+    navigate(`/home/${codeText}`);
+  };
 
   return (
     <div>
@@ -25,8 +36,14 @@ export const Home = () => {
           ) : (
             <>
               <Link to="/home/createHome">Create</Link>
-              <button>Join</button>
+              <button onClick={handleJoinClick}>Join</button>
               <Link to="/home/exploreHomes/">Explore</Link>
+              {enteredCode && (
+                <div>
+                  <input type="text" placeholder="Enter details" onChange={(e) => setCodeText(e.target.value)}/>
+                  <button onClick={handleSeeDetails}>See Details</button>
+                </div>
+              )}
             </>
           )}
         </div>
