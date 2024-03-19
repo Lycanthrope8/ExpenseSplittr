79104fc73a0df8e5a6089937b6840ed06d770385
NOCKTOWL <mehedi.hasanhulknabil@gmail.com>
2024-03-20 05:23:50 +0600

    .

--- frontend/src/components/dashboards/dashComponents/spent.js
+++ frontend/src/components/dashboards/dashComponents/spent.js
@@ -3,13 +3,13 @@ import React from 'react'
 const Spent = () => {
   return (
     <div className='col-span-2 grid grid-cols-2 gap-4'>
-        <div className='p-8 rounded-3xl bg-[#b0d2c1]'>
-            <h1>Total weekly expense</h1>
-            <p>1487</p>
+        <div className='p-8 flex flex-col justify-between rounded-3xl bg-[#b0d2c1]'>
+            <h1 className='text-2xl font-bold'>Total weekly expense</h1>
+            <p className='text-6xl text-zinc-600'>৳1487</p>
         </div>
-        <div className='p-8 rounded-3xl bg-[#bab6c1]'>
-            <h1>Total monthly expense</h1>
-            <p>12, 467</p>
+        <div className='p-8 flex flex-col justify-between rounded-3xl bg-[#bab6c1]'>
+            <h1 className='text-2xl font-bold'>Total monthly expense</h1>
+            <p className='text-6xl text-zinc-600'>৳12, 467</p>
         </div>
     </div>
   )
