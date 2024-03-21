4a955be15bba5a8903e81f8aaab64b447ebc44b7
Nocktowl <mehedi.hasanhulknabil@gmail.com>
2024-03-21 21:16:19 +0600

    Refactor expense calculation and display

--- frontend/src/components/dashboards/dashComponents/spent.js
+++ frontend/src/components/dashboards/dashComponents/spent.js
@@ -4,8 +4,33 @@ import { useAuthContext } from '../../../hooks/useAuthContext';
 const Spent = () => {
   const [expenses, setExpenses] = useState([]);
   const { user } = useAuthContext();
-  let sum = expenses.reduce((a, b) => a + b.amount, 0);
-  
+  const [weeklySum, setWeeklySum] = useState(0);
+  const [monthlySum, setMonthlySum] = useState(0);
+
+  useEffect(() => {
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+    const oneMonthAgo = new Date();
+    oneMonthAgo.setDate(oneMonthAgo.getDate() - 30);
+
+    const lastWeekExpenses = expenses.filter(expense => {
+      const expenseDate = new Date(expense.createdAt);
+      return expenseDate >= oneWeekAgo;
+    });
+
+    const lastMonthExpenses = expenses.filter(expense => {
+      const expenseDate = new Date(expense.createdAt);
+      return expenseDate >= oneMonthAgo;
+    });
+
+    let weekSum = lastWeekExpenses.reduce((a, b) => a + b.amount, 0);
+    let monthSum = lastMonthExpenses.reduce((a, b) => a + b.amount, 0);
+    setWeeklySum(weekSum);
+    setMonthlySum(monthSum);
+  }, [expenses]);
+
+
   useEffect(() => {
     // dispatchEvent({type: "TOTAL_EXPENSES"});
     const fetchExpenses = async () => {
@@ -28,12 +53,12 @@ const Spent = () => {
     <div className='col-span-2 grid grid-cols-2 gap-4'>
         <div className='p-8 flex flex-col justify-between rounded-3xl bg-[#b0d2c1]'>
             <h1 className='text-2xl font-bold'>Total weekly expense</h1>
-            <p  className='text-6xl text-zinc-600'>৳{sum}</p>
+            <p  className='text-6xl text-zinc-600'>৳{weeklySum}</p>
             {/* <p className='text-6xl text-zinc-600'>৳1487</p> */}
         </div>
         <div className='p-8 flex flex-col justify-between rounded-3xl bg-[#bab6c1]'>
             <h1 className='text-2xl font-bold'>Total monthly expense</h1>
-            <p className='text-6xl text-zinc-600'>৳12, 467</p>
+            <p className='text-6xl text-zinc-600'>৳{monthlySum}</p>
         </div>
     </div>
   )
