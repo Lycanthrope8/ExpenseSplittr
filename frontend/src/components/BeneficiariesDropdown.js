262e77ee22cdd17ae320e711db54cddfd4a6ed64
Nocktowl <mehedi.hasanhulknabil@gmail.com>
2024-04-05 18:06:18 +0600

    Add react-select package to frontend dependencies

--- frontend/src/components/BeneficiariesDropdown.js
+++ frontend/src/components/BeneficiariesDropdown.js
@@ -1,90 +1,73 @@
 import React from 'react';
 // import { Box, InputLabel, Option, FormControl, Select, colors } from '@mui/material'
-import { Select, Option, Checkbox, MenuItem } from "@material-tailwind/react";
+import Select from 'react-select';
 // import { OutlinedInput, InputLabel, MenuItem, FormControl, ListItemText, Select, Checkbox } from "@mui/material"
 
 const BeneficiariesDropdown = ({ homeMembers, selectedMembers, setSelectedMembers }) => {
   // const primary = purple[500];
+  const handleChange = (selectedMember) => {
+    setSelectedMembers(selectedMember);
+    console.log(selectedMembers);
+};
 
-
-
-  return (
-    <div className="flex flex-col gap-6 mb-4 [&_label]:text-text [&_label]:w-28 [&_label]:after:bg-main [&_label]:before:border-0 [&_label]:before:mr-0  [&_label]:after:border-0 [&_ul]:bg-secondary [&_ul]:border-none [&_li]:text-text [&_button]:text-text [&_button]:text-lg">
-        <Select
-          id = "memberId"
-          value = {selectedMembers}
-          variant='outlined'
-          label = "Members"
-          onChange = {(e) => setSelectedMembers(e)}
-          className = "p-2 h-10 w-full border-1 border-border rounded-md"
-        >
-        {/* <Option className='text-text hover:bg-secondary/80'>Select a tag</Option> */}
-        {homeMembers.map((member, index) => (
-          <Option className='mb-2 hover:bg-secondary/80'
-            key={index}
-            value={member.userId}>
-            <Checkbox checked={index > -1} />
-            {member.name}
-          </Option>
-        ))}
-        </Select>
+return (
+    <div className="mb-4">
+        <label htmlFor="beneficiaries" className="text-sm text-textext">Beneficiaries</label>
+        {homeMembers && homeMembers.length > 0 && (
+            <Select
+                id="beneficiaries"
+                isMulti
+                options={homeMembers.map((member) => ({ value: member.userId, label: member.name }))}
+                value={selectedMembers}
+                onChange={handleChange}
+                styles={{
+                    control : (styles) => {
+                        return {
+                            ...styles,
+                            backgroundColor: 'transaparent',
+                            borderColor: 'hsl(240, 3.7%, 15.9%)',
+                            borderWidth: '1px',
+                            borderRadius: '0.375rem',
+                            boxShadow: 'none',
+                            '&:hover': {
+                                borderColor: 'hsl(240, 3.7%, 15.9%)',
+                            },
+                            '&:focus': {  
+                                borderColor: 'white',
+                            },
+                        };
+                    },
+                    option: (styles) => {
+                        return {
+                            ...styles,
+                            color: 'rgb(9, 9, 11)',
+                            backgroundColor: 'hsl(240 3.7% 15.9%)',
+                            '&:hover': {
+                                backgroundColor: 'hsl(240 3.7% 15.9% / 80%)',
+                                color: '#FAFAFA',
+                            },
+                        };
+                    },
+                    multiValue: (styles) => {
+                        return {
+                            ...styles,
+                            backgroundColor: 'hsl(240 3.7% 15.9% / 80%)',
+                        };
+                    },
+                    multiValueLabel: (styles) => {
+                        return {
+                            ...styles,
+                            color: '#FAFAFA',
+                        };
+                    },
+                }}
+            />
+        )}
     </div>
-  );
+);
 
-//     const MenuProps = {};
 
-//     const members = [];   
-//     homeMembers.map((member) => (
-//         members.push(member.name)
-//     ));
-//     console.log(members);
-//     console.log(homeMembers);
-
-//   const [memberName, setMemberName] = React.useState([]);
-
-//   const handleChange = (event) => {
-//     const {
-//       target: { value },
-//     } = event;
-//     setMemberName(
-//       // On autofill we get a stringified value.
-//       typeof value === 'string' ? value.split(',') : value,
-//     );
-
-//     const MenuProps = {
-//         PaperProps: {
-//             style: {
-//             color: 'white',
-//             maxHeight: 48 * 4.5 + 8,
-//             width: 250,
-//             },
-//         },
-//         };
-//   };
-
-//   return (
-//     <div className="flex flex-col gap-6 mb-4 [&_label]:text-text [&_label]:w-28 [&_label]:after:bg-main [&_label]:before:border-0 [&_label]:before:mr-0  [&_label]:after:border-0 [&_ul]:bg-secondary [&_ul]:border-none [&_li]:text-text [&_button]:text-text [&_button]:text-lg">
-//       <FormControl sx={{ m: 1, width: 300 }}>
-//         <InputLabel color='success'>Members</InputLabel>
-//         <Select
-//           id="members"
-//           multiple
-//           value={memberName}
-//           onChange={handleChange}
-//           input={<OutlinedInput label="Members" />}
-//           renderValue={(selected) => selected.join(', ')}
-//           MenuProps={MenuProps}
-//         >
-//           {members.map((member) => (
-//             <MenuItem key={member} value={member}>
-//               <Checkbox checked={memberName.indexOf(member) > -1} />
-//               <ListItemText primary={member} />
-//             </MenuItem>
-//           ))}
-//         </Select>
-//       </FormControl>
-//     </div>
-//   );
+  
 };
 
 export default BeneficiariesDropdown;
