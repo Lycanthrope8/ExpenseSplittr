4aed206fb75fec7f3d71cab483a5e431e00a4cfc
Nocktowl <mehedi.hasanhulknabil@gmail.com>
2024-04-10 11:41:08 +0600

    Refactor Sidebar.js to add handleHomeClick function for navigating to home page

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,20 +1,23 @@
 import { createContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBangladeshiTakaSign } from '@fortawesome/free-solid-svg-icons'
 import { create } from '@mui/material/styles/createTransitions';
 
 const Sidebar = ({active}) => {
-
+    const navigate = useNavigate();
     const [expanded, setExpanded] = useState(false);
     const SidebarContext = createContext();
-
+    const handleHomeClick = () => {
+        navigate('/');
+    }
     return (
         <div className='inline-flex h-screen py-2'>
             <div className={`h-full flex flex-col bg-blue-gray-900 rounded-lg shadow-sm`}>
                 {/* <SidebarContext.Provider value={{expanded}}> */}
                 {/* {expanded ? <span className='material-symbols-rounded'>chevron_right</span> : <span className='material-symbols-rounded'>chevron_left</span>} */}
                 <ul className="flex-1 p-3 space-y-4" onMouseEnter={() => setExpanded(curr => !curr)} onMouseLeave={() => setExpanded(curr => !curr)}>
-                    <li className={`flex items-center justify-evenly text-text px-4 py-4 my-1 font-medium rounded-md cursor-pointer transition-color
+                    <li onClick={handleHomeClick} className={`flex items-center justify-evenly text-text px-4 py-4 my-1 font-medium rounded-md cursor-pointer transition-color
                         ${
                             active ? 'bg-blue-gray-800 text-white' : 'hover:bg-blue-gray-800 hover:text-white'
                         }
