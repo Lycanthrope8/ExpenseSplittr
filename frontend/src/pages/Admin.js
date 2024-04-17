87fabe9fa684e59d91b36a883f6c0217b990e0d4
Farhan-labib <farhan.labib.jahin@g.bracu.ac.bd>
2024-04-17 15:57:52 +0600

    need some design for admin panel

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,19 +1,18 @@
-import { useState, useEffect } from 'react';
-import { useAuthContext } from '../hooks/useAuthContext'
+import React, { useState, useEffect } from 'react';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export const Admin = () => {
   const [data, setData] = useState([]);
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
+
   useEffect(() => {
-    console.log('Fetching data...');
     const fetchData = async () => {
       try {
-        const response = await fetch('/geteverything', {
-            headers: { Authorization: `Bearer ${user.token}` }
-          }); 
+        const response = await fetch('/admin/geteverything', {
+          headers: { Authorization: `Bearer ${user.token}` }
+        });
         if (response.ok) {
           const data = await response.json();
-          console.log('Data fetched:', data);
           setData(data);
         } else {
           throw new Error('Failed to fetch data');
@@ -24,25 +23,27 @@ export const Admin = () => {
     };
 
     fetchData();
-  }, []);
-
-  console.log('Rendered component with data:', data);
+  }, [user.token]);
 
   return (
     <>
-      <div className='text-3xl border-b-1 mb-2' style={{ height: '50px', overflowY: 'auto' }}>
+      <div className='text-3xl border-b-1 mb-2'>
         <h1 className="text-center">Admin Panel</h1>
       </div>
-      <div className="text-center" style={{ height: '300px', overflowY: 'auto' }}>
+      <div className="text-center" >
         {/* Render data from all models */}
         {data.map((modelData, index) => (
           <div key={index} className='mb-4'>
             <h2 className='text-3xl border-b-1 mb-2'>Data from Model {index + 1}</h2>
-            {Object.entries(modelData).map(([key, value]) => (
-              <div key={key} className="expense-details flex justify-between text-white bg-secondary-dark-bg p-4 rounded-2xl">
+            {modelData.map((item, itemIndex) => (
+              <div key={itemIndex} className="expense-details flex justify-between text-white bg-secondary-dark-bg p-4 rounded-2xl">
                 <div className='flex flex-col'>
-                  <h4 className='text-3xl border-b-1 mb-2'>{key}</h4>
-                  <pre>{JSON.stringify(value, null, 2)}</pre>
+                  {Object.entries(item).map(([key, value]) => (
+                    <div key={key}>
+                      <h4 className='text-3xl border-b-1 mb-2'>{key}</h4>
+                      <pre>{JSON.stringify(value, null, 2)}</pre>
+                    </div>
+                  ))}
                 </div>
               </div>
             ))}
@@ -52,3 +53,4 @@ export const Admin = () => {
     </>
   );
 };
+
